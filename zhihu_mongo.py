8f5a1544dd35c42a8a97788b7926f4773d948a50
chaozh <chao@hust.edu.cn>
2017-08-01 10:12:48 +0800

    fix post bugs

--- zhihu_mongo.py
+++ zhihu_mongo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-from zhihu import User
+from zhihu
 from pymongo import MongoClient
 
 import logging
@@ -20,24 +20,38 @@ def main():
     db_name = 'zhihu_' + uname
     db = client[db_name]
     # could try multi-thread
-    user = User('https://www.zhihu.com/people/' + uname + '/')
+    user = zhihu.User('https://www.zhihu.com/people/' + uname + '/')
     for collection in user.get_collections():
         col_set = db[collection.id] # chinese
         
         for answer in collection.get_all_answers():
-            # also deal with post
-            question = answer.get_question()
             author = answer.get_author()
-
-            ans_obj = { 
-                "col_name": collection.get_name(), 
-                "q_title":  question.get_title(), 
-                "q_answers_num": question.get_answers_num(),
-                "q_followers_num": question.get_followers_num(),
-                "a_author": author.get_user_id(),
-                "a_upvote": answer.get_upvote(),
-                "a_content": str(answer.get_content())
-            }
+            # also deal with post
+            if isinstance(answer, zhihu.Answer):
+                question = answer.get_question()
+                ans_obj = { 
+                    "col_name": collection.get_name(),
+                    "a_type": "answer",
+                    "q_title":  question.get_title(), 
+                    "q_answers_num": question.get_answers_num(),
+                    "q_followers_num": question.get_followers_num(),
+                    "a_author": author.get_user_id(),
+                    "a_upvote": answer.get_upvote(),
+                    "a_content": str(answer.get_content())
+                }
+            else:
+                column = answer.get_column()
+                ans_obj = { 
+                    "col_name": collection.get_name(),
+                    "a_type": "post",
+                    "q_title":  column.get_title(), 
+                    "q_answers_num": column.get_posts_num(),
+                    "q_followers_num": column.get_followers_num(),
+                    "a_author": author.get_user_id(),
+                    "a_upvote": answer.get_likes(),
+                    "a_content": answer.get_content()
+                }
+            
             col_set.save(ans_obj)
 
     # collection = Collection('https://www.zhihu.com/collection/40878489', requests)
